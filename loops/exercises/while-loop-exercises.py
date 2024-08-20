53eab511e9ca6bea9f3214a45e0556c1cd45e25d
john1jung <jpjung1@gmail.com>
2024-08-20 17:40:25 -0500

    Space Shuttle Exercise

--- loops/exercises/while-loop-exercises.py
+++ loops/exercises/while-loop-exercises.py
@@ -5,7 +5,7 @@ altitude = 0
 
 # Exercise #1: Construct while loops to do the following:
   # a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. 
-while fuel_level <5000 or fuel_level > 30000:
+while fuel_level <=5000 or fuel_level > 30000:
   fuel_level = int(input("Enter Fuel Level between 5k and 30k:"))
 
 
@@ -27,7 +27,7 @@ print('The shuttle gained an altitude of', altitude, 'km and has', fuel_level, '
 
 # If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”
 if altitude >= 2000:
-   ending = 'Orbit achieved!'
+   print('Orbit achieved!')
 else:
-   ending = 'Failed to reach orbit.'
+   print('Failed to reach orbit.')
 
