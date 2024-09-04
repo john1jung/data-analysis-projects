f2154547bca46d7174357dc2a0647fc4ea1f0904
john1jung <jpjung1@gmail.com>
2024-09-03 20:15:50 -0500

    Functions Studio Reverses

--- functions/studio/functions-studio.py
+++ functions/studio/functions-studio.py
@@ -1,4 +1,13 @@
 # We want to COMPLETELY reverse a list by flipping the order of the entries AND flipping the order of characters in each element.
+
+# a) Define a 'reverse_characters' function. Give it one parameter, which will be the string to reverse.
+# b) Within the function, use the 'list' function to split a string into a list of individual characters
+# c) 'reverse' your new list.
+# d) Use 'join' to create the reversed string and return that string from the function.
+# e) Create a variable of type string to test your new function. 
+# f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+# g) Use method chaining to reduce the lines of code within the function.
+
 wordz = "This is a normal sentence."
 def reverse_characters(wordz):
     wordz = list(wordz)
@@ -7,16 +16,12 @@ def reverse_characters(wordz):
     return wordz
 print(reverse_characters(wordz))
 
-
-# a) Define a 'reverse_characters' function. Give it one parameter, which will be the string to reverse.
-# b) Within the function, use the 'list' function to split a string into a list of individual characters
-# c) 'reverse' your new list.
-# d) Use 'join' to create the reversed string and return that string from the function.
-# e) Create a variable of type string to test your new function. # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
-# g) Use method chaining to reduce the lines of code within the function.
-
+# 2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
+# a) Add an if statement to your reverse_characters function to check the typeof the parameter.
+# b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
+# e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
 wordz = 12345
-def reverse_characters(wordz):
+def reverse_characters_or_numbers(wordz):
     if type(wordz) == int: 
         wordz = str(wordz)
         wordz = list(wordz)
@@ -28,12 +33,8 @@ def reverse_characters(wordz):
         wordz.reverse()
         wordz = ''.join(wordz)
     return wordz
-print(reverse_characters(wordz))
+print(reverse_characters_or_numbers(wordz))
 
-# 2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
-# a) Add an if statement to your reverse_characters function to check the typeof the parameter.
-# b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
-# e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
 
 # 3) Create a new function with one parameter, which is the list we want to change. The function should:
 # a) Define and initialize an empty list.
@@ -42,9 +43,20 @@ print(reverse_characters(wordz))
 # d) Add the reversed string (or number) to the list defined in part ‘a’.
 # e) Return the final, reversed list.
 # f) Be sure to print the results from each test case in order to verify your code.
-
-
-
 list_test1 = ['apple', 'potato', 'Capitalized Words']
 list_test2 = [123, 8897, 42, 1168, 8675309]
 list_test3 = ['hello', 'world', 123, 'orange']
+
+
+def list_function(listz):
+    list_new = []
+    for i in listz:
+        reverse_item = reverse_characters_or_numbers(i)
+        list_new.append(reverse_item)
+    list_new.reverse()
+    return list_new
+
+
+print(list_function(list_test3))
+
+
